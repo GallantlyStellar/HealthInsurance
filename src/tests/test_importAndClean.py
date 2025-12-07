0fffa08c69825d8d627f490cee25681fa391f09c
GallantlyStellar <243000386+GallantlyStellar@users.noreply.github.com>
2025-12-07 15:46:41 -0500

    test: tests for importDF func

--- src/tests/test_importAndClean.py
+++ src/tests/test_importAndClean.py
@@ -12,6 +12,8 @@ GallantlyStellar
 
 from hashlib import file_digest
 
+from importAndClean import importDF
+
 
 def test_filehash():
     """Ensure the source data has not changed."""
@@ -20,3 +22,23 @@ def test_filehash():
     # Compare to known filehash with sha256sum of original file
     gnuShaSumsHash = "388eff679557d08ac19f463d025de5e0b4adc482537c8456d19934d78621fd47"
     assert digest == gnuShaSumsHash, "Check source data for alterations, hash mismatch"
+
+
+def test_importDF():
+    """Test properties of imported dataframe."""
+    df = importDF(path="../assets/data/raw/insurance.csv")
+    colNames = ["age", "sex", "bmi", "children", "smoker", "region", "charges"]
+    assert all(df.columns == colNames), "Column labels have changed"
+    assert all(
+        df.dtypes
+        == [
+            "uint8",
+            "uint8",
+            "float64",
+            "uint8",
+            "uint8",
+            "category",
+            "float64",
+        ]
+    ), "Dtypes have changed"
+    assert df.shape == (1338, 7), "DF shape has changed"
