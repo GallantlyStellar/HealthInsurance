93b8d12c6e179a11bd7cd1620a603a66094431f4
GallantlyStellar <243000386+GallantlyStellar@users.noreply.github.com>
2025-12-07 15:45:19 -0500

    feat: import csv file, set dtypes, clean by label encoding

---
+++ src/importAndClean.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Supports analysis for health insurance portfolio project.
+
+Modules to import and clean the insurance data.
+
+GallantlyStellar
+"""
+
+import pandas as pd
+
+
+def importDF(path: str = "../assets/data/raw/insurance.csv") -> pd.DataFrame:
+    """Format a pandas dataframe.
+
+    Parameters:
+        path: string
+            Location of the dataset to import
+
+    Returns:
+        df: pd.DataFrame
+            Formatted dataframe for further analysis
+    """
+    df = pd.read_csv(
+        "../assets/data/raw/insurance.csv",
+        dtype={
+            "age": "uint8",
+            "sex": "category",
+            "bmi": "float64",
+            "children": "uint8",
+            "smoker": "category",
+            "region": "category",
+            "charges": "float64",
+        },
+    )
+
+    # Label encode genders in alignment with ISO 5218
+    df["sex"] = (
+        df["sex"]
+        .cat.rename_categories(
+            {
+                "male": 0,
+                "female": 1,
+            }
+        )
+        .astype("uint8")
+    )
+
+    # Label encode smoker history; int not bool allows use in regression
+    df["smoker"] = (
+        df["smoker"]
+        .cat.rename_categories(
+            {
+                "no": 0,
+                "yes": 1,
+            }
+        )
+        .astype("uint8")
+    )
+
+    return df
