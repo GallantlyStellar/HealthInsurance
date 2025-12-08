a8c9b9df77bc16adebdc24780614dfbba3946122
GallantlyStellar <243000386+GallantlyStellar@users.noreply.github.com>
2025-12-08 18:16:24 -0500

    feat: Add 3d scatterplot to visualize PCs

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import seaborn as sns
 from sklearn.metrics import r2_score, root_mean_squared_error
 
@@ -27,19 +26,14 @@ plt.rcParams.update({"font.size": 20})
 
 # read in data
 df = importDF(path="../assets/data/raw/insurance.csv")
+
+# EDA
 univariate(df, 3, 3)
 bivariate(df, "charges", 2, 3)
-# plt.show()
-plt.close("all")
-
 ageChargeScatterplots(df)
-# plt.show()
-plt.close("all")
-
 encoded = oneHot(df)
-
 corr(encoded)
-plt.show()
+plt.close("all")
 
 
 # Split training and testing data
@@ -58,12 +52,22 @@ pca = pca(encoded.drop("charges", axis=1))
 pca.explained_variance_ratio_
 pcs = pca.transform(encoded.drop("charges", axis=1))
 
+# Visualize PCs compared to certain influential features
 fig = px.scatter_3d(
     x=pcs[:, 0],
     y=pcs[:, 1],
     z=df["charges"],
-    color=df["smoker"],
-    labels={"x": "PC1", "y": "PC2", "z": "Charges (USD)"},
+    symbol=(df["bmi"] > 30).replace({False: "Not Obese", True: "Obese"}),
+    color=df["smoker"].replace({0: "Nonsmoker", 1: "Smoker"}),
+    labels={
+        "x": "Principal Component 1",
+        "y": "Principal Component 2",
+        "z": "Charges (USD)",
+        "color": "Color: Smoker Status",
+        "symbol": "Symbol: BMI>30",
+    },
+    opacity=0.8,
+    title="Dimensionalty-Reduced Features vs Charges",
 )
-
+fig = fig.update_traces(marker=dict(size=6))
 fig.write_html("../report/figures/pcs.html")
