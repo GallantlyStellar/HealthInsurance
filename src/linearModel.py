eb95f46ba2f6fce5f3834e9c4803530da238e965
GallantlyStellar <243000386+GallantlyStellar@users.noreply.github.com>
2025-12-08 01:09:25 -0500

    feat: perform multiple linear regression and PCA, then plot it

---
+++ src/linearModel.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Supports analysis for health insurance portfolio project.
+
+Fit linear model.
+
+GallantlyStellar
+"""
+
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def oneHot(df: pd.DataFrame) -> pd.DataFrame:
+    """One-hot encode labels, dropping one instance to prevent multicolinearity"""
+    return pd.get_dummies(df, drop_first=True)
+
+
+def splitData(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """Split encoded DF into train and testing sets."""
+    return train_test_split(
+        df.drop("charges", axis=1),
+        df["charges"],
+        test_size=0.2,
+        random_state=7406,
+    )
+
+
+def linearModel(X_train: pd.DataFrame, y_train: pd.Series) -> LinearRegression:
+    """Fit multiple linear regression model to the encoded, split training data."""
+    return LinearRegression().fit(X_train, y_train)
+
+
+def pca(df: pd.DataFrame) -> PCA:
+    """Standardize the data and perform principal component analysis."""
+    scaled = StandardScaler().fit_transform(df.drop("charges", axis=1))
+    return PCA().fit(scaled)
--- src/main.py
+++ src/main.py
@@ -10,11 +10,16 @@ GallantlyStellar
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import seaborn as sns
+from sklearn.metrics import r2_score, root_mean_squared_error
 
 from eda import ageChargeScatterplots
 from importAndClean import importDF
+from linearModel import linearModel, oneHot, pca, splitData
 from visualize import bivariate, univariate
 
 # Make plot fonts bigger
@@ -28,4 +33,26 @@ bivariate(df, "charges", 2, 3)
 plt.close("all")
 
 ageChargeScatterplots(df)
-plt.show()
+# plt.show()
+plt.close("all")
+
+encoded = oneHot(df)
+X_train, X_test, y_train, y_test = splitData(encoded)
+lr = linearModel(X_train, y_train)
+root_mean_squared_error(y_test, lr.predict(X_test))
+r2_score(y_test, lr.predict(X_test))
+
+pca = pca(encoded)
+# pca.explained_variance_ratio_
+pcs = pca.transform(encoded.drop("charges", axis=1))
+# TODO: add line
+
+fig = px.scatter_3d(
+    x=pcs[:, 0],
+    y=pcs[:, 1],
+    z=df["charges"],
+    color=df["smoker"],
+    labels={"x": "PC1", "y": "PC2", "z": "Charges (USD)"},
+)
+
+fig.write_html("../report/figures/pcs.html")
